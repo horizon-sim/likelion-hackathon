70c9e74665d4920da45f6260f9b54d6ced761a77
hyun1999 <dazzle1999@naver.com>
2022-08-18 21:39:15 +0900

    fix:resver data modimodi

--- src/api/reserve.js
+++ src/api/reserve.js
@@ -160,7 +160,6 @@ router.post("/:shopId/:petId", verifyToken, async (req, res) => {
         if(orderCheck.length != 0){
             for(let i = 0; i < orderCheck.length; i++) {
                 let addDate = add(orderCheck[i].orderDate, {
-                    days: 30,
                     hours: 9
                 })
 
