40a6b150f7842bae8d0b5f0ef3df163f518d7987
hyun1999 <dazzle1999@naver.com>
2022-08-20 02:35:23 +0900

    asdfadsfasdf

--- src/api/time.js
+++ src/api/time.js
@@ -25,12 +25,12 @@ router.get("/:shopId", verifyToken, async (req, res) => {
         const returnData = [];
         workTime.map((noDate, index) => {
             console.log(noDate.noDate);
-            const month1 = getMonth(add(noDate.noDate, {hours: -9, months: 1}));
+            const month1 = getMonth(add(noDate.noDate, {hours: 9, months: 1}));
             const day1 = getDate(add(noDate.noDate, {hours: 9}));
             console.log(month1,month, day1, day);
             if(month1 == month && day1 == day){
                 returnData.push(getHours(add(noDate.noDate, {
-                    hours: -9
+                    hours: 9
                 })))
             };
         });
